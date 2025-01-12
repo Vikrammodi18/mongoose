43cf4e71c5df617f55f8018afb4882446dc851d3
Vikrammodi18 <suchit.sbd.9631@gmail.com>
2025-01-13 00:51:50 +0530

    created model and thier schema and added documents

--- index.js
+++ index.js
@@ -7,5 +7,21 @@ main()
 .catch((err)=> console.log(err))
 
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/FirstDB')
+    await mongoose.connect('mongodb://127.0.0.1:27017/Test')
 }
+const userSchema = mongoose.Schema({
+    name:String,
+    email:String,
+    age:Number,
+})
+
+const User = mongoose.model('User',userSchema)
+
+const user2 = User({
+    name : "Eve",
+    email: "adam123@gmail.com",
+    age : 25,
+})
+user2.save().then((res)=>
+    console.log(res)
+).catch((err)=>console.log(err))
